e3ee31eaa4189cc11aa560ef22ae71796a4f1b9c
Abduselam Awel <abdselam676@gmail.com>
2026-09-21 18:36:07 +0300

    add json file accessing functions

---
+++ src/utils/json.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const BASE_DIR = path.join(process.cwd(), "data");
+
+module.exports.readJson = async (type) => {
+  if (["users", "projects"].includes(type)) {
+    throw new Error("Invalid type name");
+  }
+
+  const fileName = path.join(BASE_DIR, `${type}.json`);
+  const isFile = await isFileExist(fileName);
+
+  if (!isFile) {
+    await writeJson([], type);
+
+    return [];
+  }
+};
+
+module.exports.writeJson = async (data, type) => {
+  const stringify = JSON.stringify(data);
+  if (["users", "projects"].includes(type)) {
+    throw new Error("Invalid type name");
+  }
+
+  const fileName = path.join(BASE_DIR, `${type}.json`);
+
+  await createDir();
+
+  await fs.writeFile(fileName, stringify);
+};
+
+const isFileExist = async (file) => {
+  try {
+    await fs.access(file);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const createDir = async () => {
+  fs.mkdir(BASE_DIR, {
+    recursive: true,
+  });
+};
